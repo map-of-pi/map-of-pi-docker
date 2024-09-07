7a9237094a8958ea54f39c46cf2baa72049d1b3e
Tate Mauzy <tate@socialchain.app>
2024-09-06 18:14:20 -0700

    Add 'console' (actually file) logging to BE prod environment

--- backend/src/config/loggingConfig.ts
+++ backend/src/config/loggingConfig.ts
@@ -9,14 +9,17 @@ export const getLoggerConfig = (): { level: string; format: any; transports: any
   let logFormat: any;
   const loggerTransports: any[] = [];
 
+  const consoleLogFormat = format.combine(format.colorize(), format.simple());
+  const consoleLogTransport = new transports.Console({ format: consoleLogFormat });
   if (env.NODE_ENV === 'development' || env.NODE_ENV === 'sandbox') {
     logLevel = 'info';
-    logFormat = format.combine(format.colorize(), format.simple());
-    loggerTransports.push(new transports.Console({ format: logFormat }));
+    logFormat = consoleLogFormat;
+    loggerTransports.push(consoleLogTransport);
   } else if (env.NODE_ENV === 'production') {
     logLevel = 'error';
     logFormat = format.combine(format.timestamp(), format.json());
-    loggerTransports.push(new SentryTransport({ stream: process.stdout }));
+    loggerTransports.push(new SentryTransport({ stream: process.stdout })); // Log to Sentry
+    loggerTransports.push(consoleLogTransport); // Log to pod as well
   } 
 
   return { level: logLevel, format: logFormat, transports: loggerTransports };
