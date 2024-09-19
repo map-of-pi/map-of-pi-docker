a03203dad293831fb72b48b84dd85b8534ab7898
swoocn <swooshinn@gmail.com>
2024-09-19 09:32:30 -0400

    Recover recent Docker backend changes.

--- backend/src/config/loggingConfig.ts
+++ backend/src/config/loggingConfig.ts
@@ -5,20 +5,24 @@ import { SentryTransport } from "./sentryConnection";
 
 // define the logging configuration logic
 export const getLoggerConfig = (): { level: string; format: any; transports: any[] } => {
-  let logLevel: string = '';
+  let defaultLogLevel: string = '';
   let logFormat: any;
   const loggerTransports: any[] = [];
 
+  const consoleLogFormat = format.combine(format.colorize(), format.simple());
+  const consoleLogTransport = new transports.Console({ format: consoleLogFormat });
   if (env.NODE_ENV === 'development' || env.NODE_ENV === 'sandbox') {
-    logLevel = 'info';
-    logFormat = format.combine(format.colorize(), format.simple());
-    loggerTransports.push(new transports.Console({ format: logFormat }));
+    defaultLogLevel = 'info';
+    logFormat = consoleLogFormat;
+    loggerTransports.push(consoleLogTransport);
   } else if (env.NODE_ENV === 'production') {
-    logLevel = 'error';
+    defaultLogLevel = 'error';
     logFormat = format.combine(format.timestamp(), format.json());
-    loggerTransports.push(new SentryTransport({ stream: process.stdout }));
+    loggerTransports.push(new SentryTransport({ stream: process.stdout })); // Log to Sentry
+    loggerTransports.push(consoleLogTransport); // Log to pod as well
   } 
 
+  const logLevel = env.LOG_LEVEL || defaultLogLevel;
   return { level: logLevel, format: logFormat, transports: loggerTransports };
 };
 
