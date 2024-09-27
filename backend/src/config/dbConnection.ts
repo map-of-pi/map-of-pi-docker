da06af0a83e30ea3b374563a5c929aa7cd971001
Danny Lee <72060658+swoocn@users.noreply.github.com>
2024-09-27 18:16:46 -0400

    Update and resolve error logging and minimize information in error responses; Resolve seller type. (#12)
    * Backend changes applied for updating and resolving error logging + response.
    
    * Frontend changes applied for adjusting error logging.
    
    * Update seller type options for proper language translation.

--- backend/src/config/dbConnection.ts
+++ backend/src/config/dbConnection.ts
@@ -17,6 +17,10 @@ export const connectDB = async () => {
     await mongoose.connect(mongodbUrl);
     logger.info("Successful connection to MongoDB.");
   } catch (error: any) {
-    logger.error(`Failed connection to MongoDB: ${error.message}`);
+    logger.error('Failed connection to MongoDB:', { 
+      message: error.message,
+      config: error.config,
+      stack: error.stack
+    });
   }
 };
