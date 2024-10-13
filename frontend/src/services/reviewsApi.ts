0f23b67290efcc1df97965e8f67fb314f6c699f6
swoocn <swooshinn@gmail.com>
2024-10-12 20:11:34 -0400

    Frontend changes to include search bar in Reviews screen.

--- frontend/src/services/reviewsApi.ts
+++ frontend/src/services/reviewsApi.ts
@@ -28,10 +28,12 @@ export const fetchSingleReview = async (reviewID: string) => {
 };
   
 // Fetch reviews for a seller
-export const fetchReviews = async (userId:string) => {
+export const fetchReviews = async (userId:string, searchQuery:string='') => {
   try {
     logger.info(`Fetching reviews for seller with UID: ${userId}`);
-    const response = await axiosClient.get(`/review-feedback/${userId}`);
+    const response = await axiosClient.get(`/review-feedback/${userId}`, {
+      params: { searchQuery },
+    });
     if (response.status === 200) {
       logger.info(`Fetch reviews successful with Status ${response.status}`, {
         data: response.data
